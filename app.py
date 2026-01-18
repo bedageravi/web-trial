b0e0ab8e25d12ccc1458d68b2f74312fac011c9d
bedageravi <bedageravi605@gmail.com>
2026-01-18 19:13:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ selected_image = random.choice(IMAGE_LIST)
 # =============================
 # TOP IMAGE (MEDIUM SIZE)
 # =============================
-st.image(selected_image, width=100)
+st.image(selected_image, width=10)
 
 # =============================
 # HERO TEXT
