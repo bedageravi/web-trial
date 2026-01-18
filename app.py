5af89b80d7d6b41034b4191575f94c0ce1b7c8b5
bedageravi <bedageravi605@gmail.com>
2026-01-18 18:11:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,10 +56,6 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-
-})
-st.dataframe(df_positions, width='stretch')
-
 # =============================
 # SESSION INIT
 # =============================
